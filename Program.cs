1e256ddeebe92e1309e2fc26a22d4431b50efc64
John Eric Magat <johnericmagat@gmail.com>
2024-02-01 09:30:58 +0800

    Singleton with test data

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+using design_patterns.Singleton;
+
+Thread t1 = new Thread(() =>
+{
+	var instance = UploadFileService.Instance(1);
+	Console.WriteLine(instance.Id);
+});
+
+Thread t2 = new Thread(() =>
+{
+	var instance = UploadFileService.Instance(2);
+	Console.WriteLine(instance.Id);
+});
+
+t1.Start();
+t2.Start();
+
+t1.Join();
+t2.Join();
--- Singleton/UploadFileService.cs
+++ Singleton/UploadFileService.cs
@@ -6,14 +6,17 @@ using System.Threading.Tasks;
 
 namespace design_patterns.Singleton
 {
-	class UploadFileService
+	// use sealed access modifier to not able to inherit
+	sealed class UploadFileService
 	{
 		private UploadFileService() {}
+
+		public int Id { get; set; }
 
 		private static UploadFileService instance;
 		private static readonly object isLocked = new object();
 
-		public static UploadFileService Instance()
+		public static UploadFileService Instance(int id)
 		{
 			if (instance == null)
 			{
@@ -22,10 +25,9 @@ namespace design_patterns.Singleton
 					if (instance == null)
 					{
 						instance = new UploadFileService();
+						instance.Id = id;
 					}
 				}
-
-				instance = new UploadFileService();
 			}
 
 			return instance;
