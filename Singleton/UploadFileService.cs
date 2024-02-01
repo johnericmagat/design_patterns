8ae17e0637ed055d75b2a610332987f568f77973
John Eric Magat <johnericmagat@gmail.com>
2024-02-01 09:17:39 +0800

    Singleton updates

---
+++ Singleton/UploadFileService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace design_patterns.Singleton
+{
+	class UploadFileService
+	{
+		private UploadFileService() {}
+
+		private static UploadFileService instance;
+		private static readonly object isLocked = new object();
+
+		public static UploadFileService Instance()
+		{
+			if (instance == null)
+			{
+				lock (isLocked)
+				{
+					if (instance == null)
+					{
+						instance = new UploadFileService();
+					}
+				}
+
+				instance = new UploadFileService();
+			}
+
+			return instance;
+		}
+	}
+}
